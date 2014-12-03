8f7e373e1b2aec56c157a39363690dbd3720d127
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-03 12:07:03 -0500

    modify seeds to function properly, update _chatbox partial to display user names and messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require 'faker'
 
-50.times { User.create(permanent_name: Faker::Internet.user_name, password: 'password') }
-50.times { Alias.create(adjective: 'fake', noun: Faker::Name.title, user: User.all.sample ) }
+50.times { Alias.create(adjective: 'fake', noun: Faker::Name.title) }
+available_aliases = Alias.all.shuffle
+50.times { User.create(permanent_name: Faker::Internet.user_name, password: 'password', alias: available_aliases.pop) }
 200.times { User.all.sample.messages << Message.new(body: Faker::Lorem.sentence) }
 30.times { User.all.sample.invitations << Invitation.new(code: '12345')}
